20cc801204c32399c5681b50c8b33c9980fc91f5
emirrssen <emirrssen@gmail.com>
2025-12-09 21:25:54 +0300

    feat: add new project for gRPC learning

--- Learning.Console/Program.cs
+++ Learning.Console/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Learning.AsynchronousAndMultithreadingProgramming._12_ThreadLocalStorage;
+using TaskFactory = Learning.AsynchronousAndMultithreadingProgramming._15_TaskFactory_TaskScheduler_TaskCompletionSource.TaskFactory;
 
-GetData_SetData example = new();
-example.Run();
+TaskFactory example = new();
+example.Run(TaskFactory.MethodToRun.ContinueWhenAny);
+Console.ReadKey();
