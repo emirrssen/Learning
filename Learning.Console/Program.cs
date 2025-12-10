83d6553a836a375da30689de0d8adc7b0540e271
Emirhan SEN <emirhan.sen@gurmen.com.tr>
2025-12-10 16:19:56 +0300

    fix: change base directory structure of the project for better maintainability

--- Learning.Console/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿using Learning.AsynchronousAndMultithreadingProgramming._12_ThreadLocalStorage;
-using TaskFactory = Learning.AsynchronousAndMultithreadingProgramming._15_TaskFactory_TaskScheduler_TaskCompletionSource.TaskFactory;
-
-TaskFactory example = new();
-example.Run(TaskFactory.MethodToRun.ContinueWhenAny);
-Console.ReadKey();
